e4ea3227953721ab5f6db21ce2ca64d5af637872
LuisPinto17 <lapinto.17@est.ucab.edu.ve>
2022-10-08 19:45:42 +0000

    commit inicial de lista de puertos

--- src/front/js/pages/single.js
+++ src/front/js/pages/single.js
@@ -4,25 +4,92 @@ import { Link, useParams } from "react-router-dom";
 import { Context } from "../store/appContext";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
 
-export const Single = props => {
-	const { store, actions } = useContext(Context);
-	const params = useParams();
+export const Single = (props) => {
+  const { store, actions } = useContext(Context);
+  const params = useParams();
+  const [address, setAddress] = useState([]);
 
-	return (
-		<div className="jumbotron">
-			<h1 className="display-4">This will show the demo element: {store.demo[params.theid].title}</h1>
-			<img src={rigoImageUrl} />
-			<hr className="my-4" />
+  useEffect(() => {
+    const getToDo = async () => {
+      let response = await fetch("https://api.ipify.org?format=json", {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        method: "GET",
+      });
+      let data = await response.json();
+      setAddress(data);
+    };
+    getToDo();
+  }, []);
 
-			<Link to="/">
-				<span className="btn btn-primary btn-lg" href="#" role="button">
-					Back home
-				</span>
-			</Link>
-		</div>
-	);
+  return (
+    <div className="jumbotron">
+      <body>
+        <div class="container">
+          <div class="mx-auto col-sm-8 main-section" id="myTab" role="tablist">
+            <div class="tab-content" id="myTabContent">
+              <div
+                class="tab-pane fade show active"
+                id="list"
+                role="tabpanel"
+                aria-labelledby="list-tab"
+              >
+                <div class="card">
+                  <div class="card-header">
+                    <h4>Lista de Puertos</h4>
+                  </div>
+                  <div class="card-body">
+                    <div>
+                      <div class="table-responsive">
+                        <table
+                          id="userList"
+                          class="table table-bordered table-hover table-striped"
+                        >
+                          <thead class="thead-light">
+                            <tr>
+                              <th scope="col">Aba</th>
+                              <th scope="col">Nombre de Banco</th>
+                              <th scope="col">Ambiente</th>
+                              <th scope="col">Switch</th>
+                              <th scope="col">Producto</th>
+                              <th scope="col">Puerto</th>
+                              <th scope="col">Dirección IP de Banco</th>
+                              <th scope="col">Dirección IP de Empresa</th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            <tr>
+                              <th></th>
+                              <th></th>
+                              <th></th>
+                              <th></th>
+                              <th></th>
+                              <th></th>
+                              <th>{address.ip}</th>
+                              <th>{address.ip}</th>
+                            </tr>
+                          </tbody>
+                        </table>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </body>
+
+      <Link to="/">
+        <span className="btn btn-primary btn-lg" href="#" role="button">
+          Back home
+        </span>
+      </Link>
+    </div>
+  );
 };
 
 Single.propTypes = {
-	match: PropTypes.object
+  match: PropTypes.object,
 };
