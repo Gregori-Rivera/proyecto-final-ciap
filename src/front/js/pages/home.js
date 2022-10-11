a09b8840221ac52d768c0725e899603f75a2ecee
Gregori-Rivera <gregori.rivera@gmail.com>
2022-10-11 15:27:09 +0000

    Login creado

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -3,24 +3,53 @@ import { Context } from "../store/appContext";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
-export const Home = () => {
-	const { store, actions } = useContext(Context);
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
-			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://github.com/4GeeksAcademy/react-flask-hello/tree/95e0540bd1422249c3004f149825285118594325/docs">
-					Read documentation
-				</a>
-			</p>
-		</div>
-	);
+import { Link } from "react-router-dom";
+
+export const Home = () => {
+  const { store, actions } = useContext(Context);
+
+  return (
+    <div className="text-center mt-5" id="img-fondo">
+      <div className="user-img">
+        <img src="https://picsum.photos/id/323/100/100"></img>
+      </div>
+      <div className="Auth-form-container">
+        <form className="Auth-form">
+          <div className="Auth-form-content">
+            <h3 className="Auth-form-title">Iniciar Sesión</h3>
+            <div className="form-group mt-3" id="user-group">
+              <label>Usuario</label>
+              <input
+                type="text"
+                className="form-control mt-1"
+                placeholder="Ingrese Usuario"
+              />
+            </div>
+            <div className="form-group mt-3" id="password-group">
+              <label>Contraseña</label>
+              <input
+                type="password"
+                className="form-control mt-1"
+                placeholder="Ingrese Contraseña"
+              />
+            </div>
+            <div className="d-grid gap-2 mt-3">
+              <Link to="/demo">
+                <button type="submit" className="btn btn-primary">
+                  Ingresar
+                </button>
+              </Link>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
