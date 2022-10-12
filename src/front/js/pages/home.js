8abf71ad3ba254df90a36a8b516a8bb91407309a
sergiodsg <sergiodasilva.dev@gmail.com>
2022-10-12 16:13:23 +0000

    Renombrando Login y convirtiendo página Demo en Home de la app

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,55 +1,41 @@
-import React, { useContext } from "react";
-import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
-import "../../styles/home.css";
-
-import Form from "react-bootstrap/Form";
-import InputGroup from "react-bootstrap/InputGroup";
-import Button from "react-bootstrap/Button";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-
+import React, { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
 
-export const Home = () => {
-  const { store, actions } = useContext(Context);
+import { Context } from "../store/appContext";
 
-  return (
-    <div className="text-center" id="img-fondo">
-      <div className="user-img">
-        <img src="https://picsum.photos/id/323/100/100"></img>
-      </div>
-      <div className="Auth-form-container">
-        <form className="Auth-form">
-          <div className="Auth-form-content">
-            <h3 className="Auth-form-title">Iniciar Sesión</h3>
-            <div className="form-group mt-3" id="user-group">
-              <label>Usuario</label>
-              <input
-                type="text"
-                className="form-control mt-1"
-                placeholder="Ingrese Usuario"
-              />
-            </div>
-            <div className="form-group mt-3" id="password-group">
-              <label>Contraseña</label>
-              <input
-                type="password"
-                className="form-control mt-1"
-                placeholder="Ingrese Contraseña"
-              />
-            </div>
-            <div className="d-grid gap-2 mt-3">
-              <Link to="/demo">
-                <button type="submit" className="btn btn-primary">
-                  Ingresar
-                </button>
-              </Link>
-            </div>
-          </div>
-        </form>
-      </div>
-    </div>
-  );
+export const Home = () => {
+	const { store, actions } = useContext(Context);
+
+	return (
+		<div className="container">
+			<ul className="list-group">
+				{store.demo.map((item, index) => {
+					return (
+						<li
+							key={index}
+							className="list-group-item d-flex justify-content-between"
+							style={{ background: item.background }}>
+							<Link to={"/single/" + index}>
+								<span>Link to: {item.title}</span>
+							</Link>
+							{// Conditional render example
+							// Check to see if the background is orange, if so, display the message
+							item.background === "orange" ? (
+								<p style={{ color: item.initial }}>
+									Check store/flux.js scroll to the actions to see the code
+								</p>
+							) : null}
+							<button className="btn btn-success" onClick={() => actions.changeColor(index, "orange")}>
+								Change Color
+							</button>
+						</li>
+					);
+				})}
+			</ul>
+			<br />
+			<Link to="/login">
+				<button className="btn btn-primary">Back home</button>
+			</Link>
+		</div>
+	);
 };
---
+++ src/front/js/pages/login.js
@@ -0,0 +1,55 @@
+import React, { useContext } from "react";
+import { Context } from "../store/appContext";
+import rigoImageUrl from "../../img/rigo-baby.jpg";
+import "../../styles/home.css";
+
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+import { Link } from "react-router-dom";
+
+export const Login = () => {
+  const { store, actions } = useContext(Context);
+
+  return (
+    <div className="text-center" id="img-fondo">
+      <div className="user-img">
+        <img src="https://picsum.photos/id/323/100/100"></img>
+      </div>
+      <div className="Auth-form-container">
+        <form className="Auth-form">
+          <div className="Auth-form-content">
+            <h3 className="Auth-form-title">Iniciar Sesión</h3>
+            <div className="form-group mt-3" id="user-group">
+              <label>Usuario</label>
+              <input
+                type="text"
+                className="form-control mt-1"
+                placeholder="Ingrese Usuario"
+              />
+            </div>
+            <div className="form-group mt-3" id="password-group">
+              <label>Contraseña</label>
+              <input
+                type="password"
+                className="form-control mt-1"
+                placeholder="Ingrese Contraseña"
+              />
+            </div>
+            <div className="d-grid gap-2 mt-3">
+              <Link to="/">
+                <button type="submit" className="btn btn-primary">
+                  Ingresar
+                </button>
+              </Link>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
