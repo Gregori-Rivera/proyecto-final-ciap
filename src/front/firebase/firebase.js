f0d79161b0c3ec25131c1c1d8e3257f9c1985ab5
Gregori-Rivera <gregori.rivera@gmail.com>
2022-10-24 23:39:47 +0000

    se establecio conexion con Firebase

--- src/front/firebase/firebase.js
+++ src/front/firebase/firebase.js
@@ -5,32 +5,46 @@ import { collection, addDoc, getFirestore, getDocs } from "firebase/firestore";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC9xumQpgMRtORHpu4rwMv2ifladGJ8ynY",
-  authDomain: "front-end-sif.firebaseapp.com",
-  projectId: "front-end-sif",
-  storageBucket: "front-end-sif.appspot.com",
-  messagingSenderId: "357222215642",
-  appId: "1:357222215642:web:91a916471b244e90bc5ae3"
+  apiKey: "AIzaSyC4Xq-ko8auZppQv8vA6JTstqX4m8fOkow",
+  authDomain: "new1-b7cb5.firebaseapp.com",
+  projectId: "new1-b7cb5",
+  storageBucket: "new1-b7cb5.appspot.com",
+  messagingSenderId: "781157043927",
+  appId: "1:781157043927:web:33d9dfa241b022cb76aa92",
+  measurementId: "G-KDL338R80D",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+console.log(db);
 try {
   const docRef = await addDoc(collection(db, "users"), {
     first: "Ada",
     last: "Lovelace",
-    born: 1815
+    born: 1815,
   });
   console.log("Document written with ID: ", docRef.id);
 } catch (e) {
   console.error("Error adding document: ", e);
 }
 
-
-
 const querySnapshot = await getDocs(collection(db, "users"));
 querySnapshot.forEach((doc) => {
   console.log(`${doc.id} => ${doc.data()}`);
 });
+
+// Your web app's Firebase configuration
+// const firebaseConfig = {
+//     apiKey: "AIzaSyATeGUnUrG1EEphDi-ZQZnEXle0rOYJDb8",
+//     authDomain: "apache-2b6c9.firebaseapp.com",
+//     projectId: "apache-2b6c9",
+//     storageBucket: "apache-2b6c9.appspot.com",
+//     messagingSenderId: "290191644000",
+//     appId: "1:290191644000:web:11e0130c5aca9dc25e3a05"
+//   };
+
+//   // Initialize Firebase
+//   const app = initializeApp(firebaseConfig);
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,56 +1,101 @@
+import { initializeApp } from "firebase/app";
+import { collection, addDoc, getFirestore, getDocs } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC4Xq-ko8auZppQv8vA6JTstqX4m8fOkow",
+  authDomain: "new1-b7cb5.firebaseapp.com",
+  projectId: "new1-b7cb5",
+  storageBucket: "new1-b7cb5.appspot.com",
+  messagingSenderId: "781157043927",
+  appId: "1:781157043927:web:33d9dfa241b022cb76aa92",
+  measurementId: "G-KDL338R80D",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+console.log(db);
+// try {
+//   const docRef = await addDoc(collection(db, "users"), {
+//     first: "Ada",
+//     last: "Lovelace",
+//     born: 1815,
+//   });
+//   console.log("Document written with ID: ", docRef.id);
+// } catch (e) {
+//   console.error("Error adding document: ", e);
+// }
+
+// const querySnapshot = await getDocs(collection(db, "users"));
+// querySnapshot.forEach((doc) => {
+//   console.log(`${doc.id} => ${doc.data()}`);
+// });
+
 const getState = ({ getStore, getActions, setStore }) => {
-	return {
-		store: {
-			message: null,
-			nav_items: [
-				{
-					title: "Mostrar información",
-					background: "white",
-					initial: "white",
-					link: "/single/"
-				},
-				{
-					title: "Agregar información",
-					background: "white",
-					initial: "white",
-					link: "/agregar/"
-				}
-			]
-		},
-		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
+  return {
+    store: {
+      message: null,
+      nav_items: [
+        {
+          title: "Mostrar información",
+          background: "white",
+          initial: "white",
+          link: "/single/",
+        },
+        {
+          title: "Agregar información",
+          background: "white",
+          initial: "white",
+          link: "/agregar/",
+        },
+      ],
+    },
+    actions: {
+      // Use getActions to call a function within a fuction
+      exampleFunction: () => {
+        getActions().changeColor(0, "green");
+      },
 
-			getMessage: async () => {
-				try{
-					// fetching data from the backend
-					const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
-					const data = await resp.json()
-					setStore({ message: data.message })
-					// don't forget to return something, that is how the async resolves
-					return data;
-				}catch(error){
-					console.log("Error loading message from backend", error)
-				}
-			},
-			// changeColor: (index, color) => {
-			// 	//get the store
-			// 	const store = getStore();
+      getCities: async () => {
+        const citiesCol = collection(db, "users");
+        const citySnapshot = await getDocs(citiesCol);
+        console.log(citySnapshot);
+        const cityList = citySnapshot.docs.map((doc) => doc.data());
+        return cityList;
+      },
 
-			// 	//we have to loop the entire demo array to look for the respective index
-			// 	//and change its color
-			// 	const demo = store.demo.map((elm, i) => {
-			// 		if (i === index) elm.background = color;
-			// 		return elm;
-			// 	});
+      getMessage: async () => {
+        try {
+          // fetching data from the backend
+          const resp = await fetch(process.env.BACKEND_URL + "/api/hello");
+          const data = await resp.json();
+          setStore({ message: data.message });
+          // don't forget to return something, that is how the async resolves
+          return data;
+        } catch (error) {
+          console.log("Error loading message from backend", error);
+        }
+      },
+      // changeColor: (index, color) => {
+      // 	//get the store
+      // 	const store = getStore();
 
-			// 	//reset the global store
-			// 	setStore({ demo: demo });
-			// }
-		}
-	};
+      // 	//we have to loop the entire demo array to look for the respective index
+      // 	//and change its color
+      // 	const demo = store.demo.map((elm, i) => {
+      // 		if (i === index) elm.background = color;
+      // 		return elm;
+      // 	});
+
+      // 	//reset the global store
+      // 	setStore({ demo: demo });
+      // }
+    },
+  };
 };
 
 export default getState;
